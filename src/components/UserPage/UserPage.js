f72991f44c61828e2a8a8b97f6fdd27a57a17280
phitranpt <phi.tran.pt@gmail.com>
2018-11-15 16:41:51 -0600

    finished base mode for total

--- src/components/UserPage/UserPage.js
+++ src/components/UserPage/UserPage.js
@@ -1,33 +1,18 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import LogOutButton from '../LogOutButton/LogOutButton';
-import axios from 'axios';
+import Total from '../Total/Total';
 
 // this could also be written with destructuring parameters as:
 // const UserPage = ({ user }) => (
 // and then instead of `props.user.username` you could use `user.username`
-
-let itemCount = 0;
-
-let fetchItemCount = (props) => {
-  console.log('going into fetchItems');
-  axios ({
-    method: 'GET',
-    url: '/count'
-  }).then((response) => {
-    console.log('response is:', response);
-  })
-}
-
-fetchItemCount();
-
 const UserPage = (props) => (
   <div>
     <h1 id="welcome">
       Welcome, { props.user.username }!
     </h1>
     <p>Your ID is: {props.user.id}</p>
-    <p>Number of items on your shelf: {itemCount}</p>
+    <Total />
     <LogOutButton className="log-in" />
   </div>
 );
